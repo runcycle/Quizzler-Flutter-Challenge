f27f49afaebe1a5eb1ef42e2039b2bfcdab746f3
runcycle <justinbartholomew@gmail.com>
2019-06-04 16:44:43 -0400

    Challenge cont

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -47,8 +47,8 @@ class QuizBrain {
 
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
   bool isFinished() {
-    if (_questionNumber >= _questionBank.length -1) {
-      print('Returning true')
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('Returning true');
       return true;
     } else {
       return false;
@@ -57,7 +57,7 @@ class QuizBrain {
   //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
 
   //TODO: Step 4 Part A - Create a reset() method here that sets the questionNumber back to 0.
-  int reset() {
+  void reset() {
     _questionNumber = 0;
   }
 }
